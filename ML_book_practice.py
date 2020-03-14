3124cafc328e793b6b845692144877f1ff205999
Sriram Narasimhan <snarasim@uwaterloo.ca>
2020-03-14 18:20:24 -0400

    added a print statement

--- ML_book_practice.py
+++ ML_book_practice.py
@@ -31,5 +31,5 @@ housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
 housing["population_per_household"] = housing["population"]/housing["households"]
 #Look at the correlation matrix
-corr_matrix = housing.corr()
-corr_matrix["median_house_value"].sort_values(ascending=False)
+housing_corr = housing.corr()
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
