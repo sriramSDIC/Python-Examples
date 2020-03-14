c70d19d955cd1a09aee2fc3bba786cce4aed3aba
Sriram Narasimhan <snarasim@uwaterloo.ca>
2020-03-14 17:33:34 -0400

    Initial commit

---
+++ ML_book_practice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 12:09:24 2020
+
+@author: snarasim
+"""
+#Convert housing.csv into a dataframe
+import pandas as pd
+import matplotlib.pyplot as plt
+
+housing = pd.read_csv("housing.csv")
+print(housing.head())
+
+#Plot the histogram
+#housing.hist(bins=50, figsize=(20,15))
+housing.hist(column='median_income', bins=50, figsize=(20,15))
+plt.show()
+# Split the data into training and test data sets
+from sklearn.model_selection import train_test_split
+train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+#copy the training data to play with
+housing = train_set.copy()
+housing.plot(kind="scatter", x="longitude", y="latitude",alpha=0.4, s=housing["population"]/100, label="population", figsize=(10,7), c=housing["median_house_value"], cmap=plt.get_cmap("jet"), colorbar=True,)
+plt.legend()
+#Correlation plots
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
