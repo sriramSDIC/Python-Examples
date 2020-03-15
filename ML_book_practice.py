7a7869236d1928a9bfa666fe2ad7e4290b236781
sriramSDIC <snarasim@uwaterloo.ca>
2020-03-15 18:42:50 -0400

    Just another commit

--- ML_book_practice.py
+++ ML_book_practice.py
@@ -32,3 +32,4 @@ housing["population_per_household"] = housing["population"]/housing["households"
 corr_matrix = housing.corr()
 print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
+
